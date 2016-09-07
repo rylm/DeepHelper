c6a68769940e051e16e5db51bb7751c4415c70a7
DTyrsa <ant5038@gmail.com>
2016-09-07 23:58:14 +0300

    Add telegram api

--- BaseNetwork/messengerApi.py
+++ BaseNetwork/messengerApi.py
@@ -1,7 +1,12 @@
+import telebot
+
+
 class BotAPI:
     def __init__(self, api_token):
-        self.api_token = api_token
+        self.telegram_token = api_token
+        self.telegram = telebot.TeleBot(self.telegram_token)
+        self.chats_id = [145718567]
 
-    @staticmethod
-    def send_messages(message):
+    def send_messages(self, message):
+        self.telegram.send_message(self.chats_id[0], message)
         print(message)
